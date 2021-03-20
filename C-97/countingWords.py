0bdccff8bb9a1594eacdfb7cfdc30142e530ee9e
Pranshu46 <72143932+Pranshu46@users.noreply.github.com>
2021-03-20 00:22:46 -0700

    Add files via upload

---
+++ C-97/countingWords.py
@@ -0,0 +1,11 @@
+introString=input("Ebter string:")
+characterCount=0
+wordCount=1
+for i in introString=characterCount+1
+    if(i==' '):
+        wordCount=wordCount+1
+
+        print("Number of word in the string:")
+        print(wordCount)
+        print("number of character in the string:")
+        print(characterCount)
